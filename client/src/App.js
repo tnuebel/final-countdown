4691b11d3a87904182a2319fbd58a7b2aaf446f7
dfwoodside <37087469+dfwoodside@users.noreply.github.com>
2018-05-15 14:19:52 -0700

    Home page updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import React, { Component } from 'react';
+import './App.css';
+// import Wrapper from "./Components/Wrapper"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+        {/* <!-- Brand --> */}
+        <a className="navbar-brand" href="#">The Final Count Down</a>
+
+        {/* <!-- Toggler/collapsibe Button --> */}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* <!-- Navbar links --> */}
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Purpose</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Mission</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Vision</a>
+            </li>
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </nav>
     );
   }
 }
+
 
 export default App;
