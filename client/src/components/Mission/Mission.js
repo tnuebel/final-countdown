2957d1a3fd6684053d61cf7b29ecbfffc48458d2
tomnuebel <tomnuebel@github.com>
2018-05-18 15:37:39 -0700

    update

--- client/src/components/Mission/Mission.js
+++ client/src/components/Mission/Mission.js
@@ -7,7 +7,7 @@ import "./Mission.css"
 
 // );
 
-class Mission extends Component {
+class Mission extends React.Component {
     render() {
         return (
             <div className="whiteTxt">
