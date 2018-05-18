d9bfc1656c6552f9d075c9365ed5eaedb0d2d468
dfwoodside <37087469+dfwoodside@users.noreply.github.com>
2018-05-18 12:49:29 -0700

    added content to About, Booking, and Greeting pages

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Navbar.css";
+import "./Navbar.css"
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 export default class Example extends React.Component {
@@ -21,15 +21,15 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar className="nav" color="faded" dark>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" id="navButton"/>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" id="nvb1"/>
           <NavbarBrand href="/" className="mr-auto">
           {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/or55UctPW4w?rel=0&amp;controls=0&amp;showinfo=0&amp;start=8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */}
-            {/* <img src="http://www.esa.int/var/esa/storage/images/esa_multimedia/videos/2016/04/the_many_faces_of_earth/15949902-1-eng-GB/The_many_faces_of_Earth_video_production_full.png" width="55" height="35" className="rounded-circle" /> */}
+            {/* <img src="http://www.esa.int/var/esa/storage/images/esa_multimedia/videos/2016/04/the_many_faces_of_earth/15949902-1-eng-GB/The_many_faces_of_Earth_video_production_full.png" width="35" height="35" className="rounded-circle" /> */}
             Final Countdown
           </NavbarBrand>
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar className="navButton">
-              <NavItem id="navpieces">
+              <NavItem>
                 <NavLink href="/">Home</NavLink>
                 <NavLink href="/About">About</NavLink>
                 <NavLink href="/Booking">Booking</NavLink>
