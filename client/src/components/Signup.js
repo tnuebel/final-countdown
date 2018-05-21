41658184ad017a803b513904fbdb9c515abd5573
dfwoodside <37087469+dfwoodside@users.noreply.github.com>
2018-05-21 14:51:44 -0700

    Added video to SignIn page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AuthService from './AuthService';
 import { Link } from 'react-router-dom';
 import YouTube from 'react-youtube';
+import './Signup.css';
 
 class Signup extends Component {
   constructor() {
@@ -20,54 +21,75 @@ class Signup extends Component {
     const opts = {
       height: '390',
       width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
+      playerVars: {         // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,        // Auto-play the video on load
+        controls: 0,        // Show pause/play buttons in player
+        showinfo: 0,        // Hide the video title
+        modestbranding: 1,  // Hide the Youtube Logo
+        fs: 1,              // Hide the full screen button
+        cc_load_policy: 0,  // Hide closed captions
+        iv_load_policy: 3,  // Hide the Video Annotations
+        autohide: 0,        // Hide video controls when playing
         start: 17,
         end: 19
       }
     };
-    return (
-      <div className="container">
-         <YouTube
-          videoId="95a2KnIWswk"
-          opts={opts}
-          onEnd={this._onEnd}
-        />
 
-        <h1>Signup</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input className="form-control"
-              placeholder="Username goes here..."
-              name="username"
-              type="text"
-              id="username"
-              onChange={this.handleChange} />
+    return (
+      <div className="video-background">
+        <div className="video-foreground">
+          <div className="container">
+            <YouTube
+              videoId="95a2KnIWswk"
+              opts={opts}
+              onEnd={this._onEnd}
+            />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email address:</label>
-            <input className="form-control"
-              placeholder="Email goes here..."
-              name="email"
-              type="email"
-              id="email"
-              onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Password:</label>
-            <input className="form-control"
-              placeholder="Password goes here..."
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        <p><Link to="/login">Go to Login</Link></p>
+        </div>
+
+        <div class="form-wrapper">
+          <h1>Signup</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input className="form-control"
+                placeholder="Username goes here..."
+                name="username"
+                type="text"
+                id="username"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email address:</label>
+              <input className="form-control"
+                placeholder="Email goes here..."
+                name="email"
+                type="email"
+                id="email"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input className="form-control"
+                placeholder="Password goes here..."
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+          <p><Link to="/login">Go to Login</Link></p>
+        </div>
       </div>
     );
+  }
+
+  _onEnd(event) {
+    // when the video end
+    // restart it from the 17 second mark 
+    console.log(event);
+    event.target.seekTo(17);
   }
 
   handleFormSubmit(e) {
@@ -88,13 +110,6 @@ class Signup extends Component {
         [e.target.name]: e.target.value
       }
     )
-  }
-  _onEnd(event) {
-    console.log(event);
-    // when the video end
-
-    // restart it from the 17 second mark 
-    event.target.seekTo(17);
   }
 }
 
